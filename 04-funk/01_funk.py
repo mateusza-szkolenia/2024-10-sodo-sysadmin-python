616eb0404a11a19725dd4098b2bc9277a0b321cb
Mateusz Adamowski <mateusz@adamowski.pl>
2024-10-17 15:29:44 +0200

    funkcja C2F

--- 04-funk/01_funk.py
+++ 04-funk/01_funk.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-def przelicz_C_na_F(temp):
+def przelicz_C_na_F(temp: float) -> float:
+    """Funkcja przelicza temperaturę ze st. C na F"""
     return 1.8 * temp + 32
 
 x = float(input("Podaj temp w C: "))
@@ -8,3 +9,9 @@ x = float(input("Podaj temp w C: "))
 x2 = przelicz_C_na_F(x)
 
 print(f"Temp w F {x2}")
+
+x3 = przelicz_C_na_F(10.0)
+
+x4 = przelicz_C_na_F()
+
+print(x3, x4)
