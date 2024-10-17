08b46934b75bed9e487cef256b8fdf552459e8f9
Mateusz Adamowski <mateusz@adamowski.pl>
2024-10-17 12:18:42 +0200

    with

---
+++ 01-pliki/04_pliki.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+Zadanie 1
+
+Napisać program podający uptime systemu w godzinach, z dokładnością do dziesiątej części.
+
+Użyć danych z /proc/uptime
+"""
+
+with open("/proc/uptime") as plik:
+    zawartosc = plik.read()
+
+a, b = zawartosc.split(" ")
+
+czas_w_sek = int(float(a))
+
+czas_w_godz = float(a) / 3600
+
+godz, minu, sek = czas_w_sek // 3600, (czas_w_sek % 3600) // 60, czas_w_sek % 60
+
+print(f"{czas_w_godz:.1f}")
+
+print(f"{godz}:{minu}:{sek}")
