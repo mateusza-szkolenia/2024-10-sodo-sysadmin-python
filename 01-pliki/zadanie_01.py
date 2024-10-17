93f318aa6a8cd9fb80b50676703b719696cba91b
Mateusz Adamowski <mateusz@adamowski.pl>
2024-10-17 11:52:30 +0200

    rozwiązanie krok 1

--- 01-pliki/zadanie_01.py
+++ 01-pliki/zadanie_01.py
@@ -8,3 +8,13 @@ Napisać program podający uptime systemu w godzinach, z dokładnością do dzie
 Użyć danych z /proc/uptime
 """
 
+plik = open("/proc/uptime")
+
+zawartosc = plik.read()
+
+a, b = zawartosc.split(" ")
+
+czas_w_godz = float(a) / 3600
+
+print(czas_w_godz)
+
