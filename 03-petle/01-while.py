0061f6fc7768e7f2265995e2cde38d1b2b714041
Mateusz Adamowski <mateusz@adamowski.pl>
2024-10-17 14:36:14 +0200

    pętle

---
+++ 03-petle/01-while.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+x = 1.0
+
+while x < 100:
+  x = x * 1.13
+  print("#" * int(x))
---
+++ 03-petle/02-for.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+imiona = ["Mateusz", "Magda", "Andrzej"]
+
+for imie in imiona:
+    print(f"Witaj {imie}")
+
